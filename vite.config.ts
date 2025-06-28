14446588ebb0cad81eec7bafb2342f126d47616c
gitanshugupta7 <gitanshugupta77@gmail.com>
2025-06-28 20:11:08 +0530

    modified vite.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }) => {
+          if (/\.(ttf|otf|woff|woff2)$/.test(name ?? "")) {
+            return "assets/[name][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
