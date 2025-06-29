180014cf62838ed2150a13da73193f7ea0ecbbf8
gitanshugupta7 <gitanshugupta77@gmail.com>
2025-06-29 15:23:35 +0530

    fixed routing

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -172,7 +172,7 @@ function Header() {
                   Group Sessions
                 </Link>
                 <Link
-                  to="/services/individual-therapy"
+                  to="/services/collectives"
                   className="block p-2 text-sm text-gray-700 hover:bg-gray-100"
                   onClick={toggleMenu}
                 >
